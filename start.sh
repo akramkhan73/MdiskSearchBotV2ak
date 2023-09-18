56bec40002b5264ffc456caeff562172c20f6962
akramkhan73 <145324243+akramkhan73@users.noreply.github.com>
2023-09-18 01:36:21 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.comQ/@2amovi/MdiskSearchBotV2ak.git /MdiskSearchBotV2ak
+git clone https://github.com/@2amovi/MdiskSearchBotV2ak.git /MdiskSearchBotV2ak
 cd /MdiskSearchBotV2ak
 pip3 install -r requirements.txt
 echo "Starting Bot...."
